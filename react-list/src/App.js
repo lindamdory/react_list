c520f2917fd711300899e5e06ade7efd8c42e148
lindamdory <66410698+lindamdory@users.noreply.github.com>
2021-02-02 11:14:02 +0100

    useState, useEffect

--- react-list/src/App.js
+++ react-list/src/App.js
@@ -1,12 +1,45 @@
-import List from './components/List';
-import './App.css';
+import List from "./components/List";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+  const [showMessage, setShowMessage] = useState(true);
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `(${count}) Inbox - ClickMail`;
+  });
+
   return (
     <>
+      <div>
+        <button onClick={() => setCount(count + 1)}>Clickmail</button>
+
+        {showMessage ? <p>You have {count} new emails.</p> : ""}
+
+        <br></br>
+
+        <button
+          onClick={() => {
+            setShowMessage(true);
+          }}
+        >
+          Show Message
+        </button>
+
+        <button
+          onClick={() => {
+            setShowMessage(false);
+          }}
+        >
+          Hide Message
+        </button>
+      </div>
+
       <h1>Hello</h1>
       <form>
-        <List/>
+        <List />
       </form>
       <h1>Bye</h1>
     </>
