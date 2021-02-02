b02db5f9cf1479a0ff9e00df8a07b3318f73efaa
lindamdory <66410698+lindamdory@users.noreply.github.com>
2021-02-02 17:13:58 +0100

    useEffect

--- react-list/src/App.js
+++ react-list/src/App.js
@@ -6,10 +6,22 @@ function App() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
   const [showMessage, setShowMessage] = useState(true);
+
+  // Update the document title using the browser API
+  // csak egy paraméter van:
   useEffect(() => {
-    // Update the document title using the browser API
     document.title = `(${count}) Inbox - ClickMail`;
   });
+
+  // // két paraméter, tömbben count változó:
+  // useEffect(() => {
+  //   document.title = `(${count}) Inbox - ClickMail`;
+  // }, [count]);
+
+  // // két paraméter van, üres tömb, csak a kezdeti értéket írja ki, nem változik?
+  // useEffect(() => {
+  //   document.title = `(${count}) Inbox - ClickMail`;
+  // }, []);
 
   return (
     <>
